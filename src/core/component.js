37e5082a958a6833d02b391aca5540924eadf27e
Pat Long <plong00@gmail.com>
2017-04-16 10:31:54 -0700

    Handling Core.Component constructor with no arguments

--- src/core/component.js
+++ src/core/component.js
@@ -1,5 +1,8 @@
 export default class Component {  
-  constructor(SL, config) {
+  constructor(SL, config={}) {
+    if (!SL) {
+      throw 'Cannot initialize StampLines component without an SL object!';
+    }
     this.SL = SL;
     this.config = config;
   }
--- test/tests/core/component-test.js
+++ test/tests/core/component-test.js
@@ -4,5 +4,10 @@ describe('Core.Component', () => {
       let component = new Test.Lib.Core.Component(Test.SL, {});
       expect(component.type).to.equal('Component');
     });
+    it('should throw an error if no StampLines object provided', () => {
+      expect(() => {
+        let component = new Test.Lib.Core.Component();
+      }).to.throw('Cannot initialize StampLines component without an SL object!');
+    });
   });
 });
