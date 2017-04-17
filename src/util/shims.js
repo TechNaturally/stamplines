38fbdef1dcf9d7233fbe322e9fdff3ade8ce6ef2
Pat Long <plong00@gmail.com>
2017-04-17 10:21:01 -0700

    Added more String shims

--- src/util/shims.js
+++ src/util/shims.js
@@ -1,3 +1,6 @@
+String.prototype.capitalizeFirstLetter = function() {
+  return this.charAt(0).toUpperCase() + this.slice(1);
+};
 String.prototype.toDashCase = function() {
   return this
           .replace(/^[A-Z]/, ($1) => {
@@ -5,5 +8,11 @@ String.prototype.toDashCase = function() {
           })
           .replace(/([A-Z])/g, ($1) => {
             return '-'+$1.toLowerCase();
+          });
+};
+String.prototype.toCamelCase = function(){
+  return this
+          .replace(/([\-_\.][a-z])/g, ($1) => {
+            return $1.toUpperCase().replace('-','');
           });
 };
