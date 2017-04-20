b37aa893f06b0fd32976671d2713d9cbe34a406e
Pat Long <plong00@gmail.com>
2017-04-20 11:07:52 -0700

    Implementing Core.Tool

--- src/core/tool.js
+++ src/core/tool.js
@@ -1,7 +1,8 @@
 import Component from './component.js';
 export default class Tool extends Component {
-  constructor(SL, config) {
+  constructor(SL, config, Belt) {
     super(SL, config);
+    this.Belt = Belt;
     this.configure();
   }
   get type() {
@@ -12,5 +13,17 @@ export default class Tool extends Component {
   }
   set name(name) {
     this._name = name;
+  }
+  get activationPriority() {
+    return -1;
+  }
+  isActive() {
+    return !!this.active;
+  }
+  activate() {
+    this.active = true;
+  }
+  deactivate() {
+    this.active = false;
   }
 }
--- test/tests/core/tool-test.js
+++ test/tests/core/tool-test.js
@@ -1,15 +1,56 @@
 describe('Core.Tool', () => {
+  let Tool;
+  before(() => {
+    Test.assertSL();
+  });
+  beforeEach(() => {
+    Tool = new Test.Lib.Core.Tool(Test.SL, {});
+  });
+  afterEach(() => {
+    Tool = undefined;
+  });
   describe('Constructor', () => {
-    let Tool;
-    before(() => {
-      Test.assertSL();
-      Tool = new Test.Lib.Core.Tool(Test.SL, {});
-    });
     it('should initialize', () => {
       expect(Tool).to.exist;
     });
     it('should be constructed by Tool', () => {
       expect(Tool.constructor.name).to.equal('Tool');
+    });
+    it('should start inactive', () => {
+      expect(Tool.active).to.not.be.true;
+    });
+  });
+  describe('+activationPriority', () => {
+    it('should equal -1', () => {
+      expect(Tool.activationPriority).to.equal(-1);
+    });
+  });
+  describe('#activate', () => {
+    it('should set active property to true', () => {
+      Tool.activate();
+      expect(Tool.active).to.be.true;
+    });
+  });
+  describe('#deactivate', () => {
+    beforeEach(() => {
+      Tool.activate();
+    });
+    it('should set active property to false', () => {
+      Tool.deactivate();
+      expect(Tool.active).to.be.false;
+    });
+  });
+  describe('#isActive', () => {
+    describe('- when tool is active', () => {
+      it('should return true', () => {
+        Tool.activate();
+        expect(Tool.isActive()).to.be.true;
+      });
+    });
+    describe('- when tool is not active', () => {
+      it('should return false', () => {
+        expect(Tool.isActive()).to.be.false;
+      });
     });
   });
 });
