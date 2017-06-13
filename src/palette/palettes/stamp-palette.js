caa2d96be0bf1d8e5904b9b47f1f200263108eac
Pat Long <plong00@gmail.com>
2017-06-13 12:35:53 -0700

    Implements Stamp Panel (and Labels)

--- src/palette/palettes/stamp-palette.js
+++ src/palette/palettes/stamp-palette.js
@@ -5,6 +5,8 @@ export default class StampPalette extends Palette {
     this.id = 'Stamps';
     this.symbols = {};
     this.PaperItems = [];
+    this.initialized = true;
+    this.configure();
   }
   get paletteType() {
     return 'Stamps';
@@ -14,6 +16,20 @@ export default class StampPalette extends Palette {
       return this.config.stamps;
     }
     return super.paletteItems;
+  }
+  configure(config) {
+    config = super.configure(config);
+    this.initEventHandlers();
+    this.registerSnappers();
+    return config;
+  }
+  reset() {
+    if (!this.initialized) {
+      return;
+    }
+    super.reset();
+    this.unregisterSnappers();
+    this.resetEventHandlers();
   }
   configureItem(item) {
     if (item.id) {
@@ -96,7 +112,167 @@ export default class StampPalette extends Palette {
     let symbol = this.getStampSymbol(item).clone();
     if (symbol) {
       let stamp = this.SL.Paper.generatePaperItem({Source: this, Type: 'Stamp', Stamp: item}, paper.SymbolItem, symbol, position);
+      this.SL.Paper.Item.addCustomMethod(stamp, 'refresh', this.refreshItem, this);
       return stamp;
+    }
+  }
+  refreshItem(item, args) {
+    if (item && item.data && item.data.label) {
+      this.assertItemLabel(item);
+      this.refreshItemLabel(item);
+    }
+    else if (item.data.paperLabel) {
+      this.removeItemLabel(item);
+    }
+  }
+
+  assertItemLabel(item) {
+    if (item && item.data && !item.data.paperLabel) {
+      let labelData = {
+        Source: this,
+        Type: 'StampLabel',
+        Class: 'ContentAddon',
+        Layer: ((item.data && item.data.Layer) ? item.data.Layer : 'CONTENT'),
+        ParentItem: item,
+        position: {x: 0, y: 0} // @TODO: is labelData.position needed?
+      };
+      // @TODO: configure Stamp.Label (position, font)
+      item.data.paperLabel = this.SL.Paper.generatePaperItem(labelData, paper.PointText, item.bounds.center);
+      item.data.paperLabel.fillColor = '#000000';
+      item.data.paperLabel.fontSize = '12pt';
+      this.SL.Paper.Item.addChild(item, item.data.paperLabel);
+    }
+  }
+  refreshItemLabel(item) {
+    if (item && item.data && item.data.paperLabel) {
+      item.data.paperLabel.content = item.data.label;
+      item.data.paperLabel.insertAbove(item);
+      let Geo = this.SL.Utils.get('Geo');
+      if (Geo && item.data.paperLabel.data && item.data.paperLabel.data.position) {
+        let rotation = item.rotation;
+        let rotationPoint = item.bounds.center;
+        if (rotation) {
+          item.rotate(-rotation, rotationPoint);
+        }
+        let point = Geo.Normalize.pointFromRectangle(item.data.paperLabel.data.position, item.bounds);
+        if (rotation) {
+          item.rotate(rotation, rotationPoint);
+          point = point.rotate(rotation, rotationPoint);
+        }
+        item.data.paperLabel.position.set(point);
+      }
+    }
+  }
+  removeItemLabel(item) {
+    if (item && item.data && item.data.paperLabel) {
+      this.SL.Paper.Item.removeChild(item, item.data.paperLabel);
+      this.SL.Paper.destroyPaperItem(item.data.paperLabel);
+      item.data.paperLabel = undefined;
+      delete item.data.paperLabel;
+    }
+  }
+
+  stampSelected(stamp) {
+    if (stamp && stamp.data && stamp.data.Type == 'Stamp') {
+      if (stamp.data.paperLabel) {
+        stamp.data.paperLabel.insertAbove(stamp);
+      }
+    }
+  }
+  stampUnselected(stamp) {
+    if (stamp && stamp.data && stamp.data.Type == 'Stamp') {
+      if (stamp.data.paperLabel) {
+        stamp.data.paperLabel.insertAbove(stamp);
+      }
+    }
+  }
+  snapItem(item, config={}) {
+    if (item && item.data && item.data.Type == 'Stamp') {
+      this.SL.Paper.Item.callCustomMethod(item, 'refresh');
+    }
+    return item;
+  }
+
+  initEventHandlers() {
+    if (!this.initialized) {
+      return;
+    }
+    if (!this.eventHandlers) {
+      this.eventHandlers = {};
+    }
+    let StampFilter = {Type: 'Stamp'};
+    if (!this.eventHandlers.ItemSelected) {
+      this.eventHandlers.ItemSelected = this.SL.Paper.on('SelectionItemSelected', StampFilter, (args, stamp) => {
+        if (!stamp && args && args.item && this.SL.Paper.Item.passesFilter(args.item, StampFilter)) {
+          stamp = args.item;
+        }
+        if (stamp && this.SL.Paper.Item.passesFilter(stamp, StampFilter)) {
+          this.stampSelected(stamp);
+        }
+      }, 'Stamp.Selected');
+    }
+    if (!this.eventHandlers.ItemUnselected) {
+      this.eventHandlers.ItemUnselected = this.SL.Paper.on('SelectionItemUnselected', StampFilter, (args, stamp) => {
+        if (args.items) {
+          args.items.forEach((stamp) => {
+            if (stamp && this.SL.Paper.Item.passesFilter(stamp, StampFilter)) {
+              this.stampUnselected(stamp);
+            }
+          });
+        }
+        else if (stamp && this.SL.Paper.Item.passesFilter(stamp, StampFilter)) {
+          this.stampUnselected(stamp);
+        }
+      }, 'Stamp.Unselected');
+    }
+    if (!this.eventHandlers.ItemDestroyed) {
+      this.eventHandlers.ItemDestroyed = this.SL.Paper.on('Destroy', StampFilter, (args, stamp) => {
+        this.removeItemLabel(stamp);
+      }, 'Stamp.Destroyed');
+    }
+  }
+  resetEventHandlers() {
+    if (!this.initialized || !this.eventHandlers) {
+      return;
+    }
+    if (this.eventHandlers.ItemSelected) {
+      this.SL.Paper.off('SelectionItemSelected', this.eventHandlers.ItemSelected.id);
+      delete this.eventHandlers.ItemSelected;
+      this.eventHandlers.ItemSelected = undefined;
+    }
+    if (this.eventHandlers.ItemUnselected) {
+      this.SL.Paper.off('SelectionItemUnselected', this.eventHandlers.ItemUnselected.id);
+      delete this.eventHandlers.ItemUnselected;
+      this.eventHandlers.ItemUnselected = undefined;
+    }
+    if (this.eventHandlers.ItemDestroyed) {
+      this.SL.Paper.off('Destroy', this.eventHandlers.ItemDestroyed.id);
+      delete this.eventHandlers.ItemDestroyed;
+      this.eventHandlers.ItemDestroyed = undefined;
+    }
+  }
+  registerSnappers() {
+    let Snap = this.SL.Utils.get('Snap');
+    if (Snap) {
+      if (!this.Snappers) {
+        this.Snappers = {};
+      }
+      this.Snappers.item = Snap.addSnapper('item', {
+        priority: 1000,
+        callback: (item, config) => {
+          return this.snapItem(item, config);
+        }
+      });
+    }
+  }
+  unregisterSnappers() {
+    let Snap = this.SL.Utils.get('Snap');
+    if (!Snap || !this.Snappers) {
+      return;
+    }
+    if (this.Snappers.item) {
+      Snap.dropSnapper('item', this.Snappers.item.id);
+      this.Snappers.item = undefined;
     }
   }
 }
--- src/panel/panels/stamp-panel.js
+++ src/panel/panels/stamp-panel.js
@@ -3,5 +3,147 @@ export default class StampPanel extends Panel {
   constructor(SL, config) {
     super(SL, config);
   }
-  // @TODO: implement StampPanel content generation
+  refreshItem(item) {
+    this.SL.Paper.Item.callCustomMethod(item, 'refresh');
+  }
+  setItemLabel(item, label) {
+    if (item) {
+      if (!item.data) {
+        item.data = {};
+      }
+      item.data.label = label;
+      this.refreshItem(item);
+    }
+  }
+  setItemLabelPosition(item, labelPosition) {
+    if (item) {
+      if (!item.data) {
+        item.data = {};
+      }
+      item.data.labelPosition = labelPosition;
+      this.refreshItem(item);
+    }
+  }
+  setData(data={}) {
+    super.setData(data);
+    if (this.data && this.data.item && this.data.item.data) {
+      let itemData = this.data.item.data;
+      if (itemData.Stamp) {
+        this.setTitle(itemData.Stamp.name || itemData.Stamp.id);
+      }
+      if (this.DOM && this.DOM.labelInput) {
+        this.DOM.labelInput.data('original', itemData.label);
+        this.DOM.labelInput.val(itemData.label);
+      }
+      if (this.DOM && this.DOM.labelPositionGroup) {
+        itemData.labelPosition = (itemData.labelPosition || 'center');
+        this.DOM.labelPositionGroup.data('original', itemData.labelPosition);
+        this.DOM.labelPositionGroup.children('input[name="item-label-position"]').val([itemData.labelPosition]);
+      }
+    }
+    this.positionPanel();
+  }
+  getPanelClass() {
+    let result = super.getPanelClass();
+    result.push('sl-stamp-panel');
+    return result;
+  }
+  generateDOM() {
+    let panel = super.generateDOM();
+    let content = this.assertDOMContent();
+    let item;
+    if (this.data && this.data.item) {
+      item = this.data.item;
+    }
+
+    // build the form
+    let form = $('<form class="sl-form"></form>');
+    form.on('submit', (event) => {
+      return false;
+    });
+    form.attr('name', this.getPanelID()+'-form');
+
+    // build form elements
+    let formGroup, formInput;
+
+    // label input
+    formGroup = $('<div class="form-group form-group-text input-item-label"></div>');
+    formInput = $('<input type="text" name="item-label" id="'+form.attr('name')+'-item-label'+'" />');
+    formGroup.append($('<label for="'+formInput.attr('id')+'">Label</label>'));
+    formGroup.append(formInput);
+    /**
+    formInput.on('change', () => {
+      if (this.data && this.data.item && this.DOM.labelInput) {
+        this.setItemLabel(this.data.item, this.DOM.labelInput.val());
+      }
+    });
+    */
+    form.append(formGroup);
+    this.DOM.labelInput = formInput;
+
+    let originalValue = '';
+    if (this.data && this.data.item && this.data.item.data && this.data.item.data.label) {
+      originalValue = this.data.item.data.label;
+    }
+    this.DOM.labelInput.data('original', originalValue);
+    this.DOM.labelInput.val(originalValue);
+
+    // label position
+    formGroup = $('<div class="form-group form-group-position input-item-label-position"></div>');
+    formInput = $('<div class="sl-position-input sl-position-5-way"></div>');
+    formInput.append($('<input type="radio" name="item-label-position" class="sl-position position-center" value="center" id="'+form.attr('name')+'-item-label-position-center" />'));
+    formInput.append($('<input type="radio" name="item-label-position" class="sl-position position-top" value="top" id="'+form.attr('name')+'-item-label-position-top" />'));
+    formInput.append($('<input type="radio" name="item-label-position" class="sl-position position-right" value="right" id="'+form.attr('name')+'-item-label-position-right" />'));
+    formInput.append($('<input type="radio" name="item-label-position" class="sl-position position-bottom" value="bottom" id="'+form.attr('name')+'-item-label-position-bottom" />'));
+    formInput.append($('<input type="radio" name="item-label-position" class="sl-position position-left" value="left" id="'+form.attr('name')+'-item-label-position-left" />'));
+    formGroup.append('<label>Label Position</label>');
+    formGroup.append(formInput);
+    form.append(formGroup);
+    this.DOM.labelPositionGroup = formInput;
+    originalValue = 'center';
+    if (this.data && this.data.item && this.data.item.data && this.data.item.data.labelPosition) {
+      originalValue = this.data.item.data.labelPosition;
+    }
+    this.DOM.labelPositionGroup.data('original', originalValue);
+    this.DOM.labelPositionGroup.children('input[name="item-label-position"]').val([originalValue]);
+
+    // buttons
+    formGroup = $('<div class="form-group form-buttons"></div>');
+    formInput = $('<button type="button" class="save"><i class="icon icon-check"></i> Save</button>');
+    formInput.on('click', (event) => {
+      if (this.data && this.data.item) {
+        if (this.DOM.labelInput) {
+          this.setItemLabel(this.data.item, this.DOM.labelInput.val());
+        }
+        if (this.DOM.labelPositionGroup) {
+          this.setItemLabelPosition(this.data.item, this.DOM.labelPositionGroup.children('input[name="item-label-position"]:checked').val());
+        }
+      }
+      this.close();
+    });
+    formGroup.append(formInput);
+
+    formInput = $('<button type="button" class="cancel"><i class="icon icon-close"></i> Cancel</button>');
+    formInput.on('click', (event) => {
+      if (this.data && this.data.item) {
+        if (this.DOM.labelInput) {
+          this.setItemLabel(this.data.item, this.DOM.labelInput.data('original'));
+        }
+        if (this.DOM.labelPositionGroup) {
+          this.setItemLabelPosition(this.data.item, this.DOM.labelPositionGroup.data('original'));
+        }
+      }
+      this.close();
+    });
+    formGroup.prepend(formInput);
+    form.append(formGroup);
+    this.resetDOMControlCloseButton();
+
+    // add the form to the panel
+    content.append(form);
+
+    this.positionPanel();
+
+    return panel;
+  }
 }
