3d12fe98b7e18b9d45eb08e4cea301ad2f2152ff
Pat Long <plong00@gmail.com>
2017-07-07 13:49:38 -0700

    Fixed typo in test descriptions

--- test/tests/util/utils/identity-test.js
+++ test/tests/util/utils/identity-test.js
@@ -11,13 +11,13 @@ describe('Utils.Identity', () => {
       };
       expect(ID.getUnique(baseID, list)).to.equal(baseID);
     });
-    it('should return a sequential ID if "baseID-1" if "baseID" exists', () => {
+    it('should return a sequential ID of "baseID-1" if "baseID" exists', () => {
       let list = {
         'test': 'foo'
       };
       expect(ID.getUnique(baseID, list)).to.equal(baseID+'-1');
     });
-    it('should return a sequential ID if "baseID-2" if "baseID" and "baseID-1" exist', () => {
+    it('should return a sequential ID of "baseID-2" if "baseID" and "baseID-1" exist', () => {
       let list = {
         'test': 'foo',
         'test-1': 'bar'
