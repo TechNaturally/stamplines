f932fa769063193751cb7a5f9072731e09d5749c
Pat Long <plong00@gmail.com>
2018-02-26 06:20:57 -0700

    Stamp export

--- src/palette/palettes/stamp-palette.js
+++ src/palette/palettes/stamp-palette.js
@@ -5,6 +5,7 @@ export default class StampPalette extends Palette {
     this.id = 'Stamps';
     this.symbols = {};
     this.PaperItems = [];
+    this.svgCache = {};
     this.initialized = true;
     this.configure();
   }
@@ -70,6 +71,62 @@ export default class StampPalette extends Palette {
       imagePath = this.SL.Utils.gets('URL').toAbsolutePath(imagePath);
       return imagePath;
     }
+  }
+  getStampSVG(item) {
+    if (item && item.data && item.data.Stamp) {
+      // build URL for the SVG
+      let domainBase = window.location.protocol + '//' + window.location.hostname + (window.location.port ? ':'+window.location.port : '');
+      let imagePath = this.getImagePath(item.data.Stamp);
+      let imageURL = domainBase + imagePath;
+
+      // if it hasn't been requested before
+      if (!this.svgCache[imageURL]) {
+        // make a new request
+        this.svgCache[imageURL] = new Promise((resolve, reject) => {
+          $.ajax(imageURL, {
+            dataType: 'xml',
+            success: (data, status, xhr) => {
+              // assign the serialized svg into the svgCache
+              let svgData = new XMLSerializer().serializeToString(data);
+              this.svgCache[imageURL] = svgData;
+            },
+            error: (xhr, status, error) => {
+              // it would try again next time
+              this.svgCache[imageURL] = undefined;
+            },
+            complete: () => {
+              // always going to resolve or reject after attempting to load
+              if (this.svgCache[imageURL]) {
+                resolve(this.svgCache[imageURL]);
+              }
+              else {
+                reject('Could not load SVG.');
+              }
+            }
+          });
+        });
+      }
+      // if it has been requested before
+      if (this.svgCache[imageURL]) {
+        // check if the version in svgCache has been resolved yet
+        if (this.svgCache[imageURL].constructor.name == 'Promise') {
+          // unresolved svgCache loader
+          return this.svgCache[imageURL];
+        }
+        else {
+          // the loader has resolved
+          // always want to return a Promise
+          return new Promise((resolve, reject) => {
+            // instantly resolve it
+            resolve(this.svgCache[imageURL]);
+          });
+        }
+      }
+    }
+    // always want to return a Promise
+    return new Promise((resolve, reject) => {
+      reject('No stamp definition found.');
+    });
   }
   getStampDef(id) {
     if (this.config && this.config.stamps) {
@@ -173,6 +230,26 @@ export default class StampPalette extends Palette {
           }
         }
       }
+    }
+  }
+  exportStamp(item, args) {
+    if (item && item.data && item.data.Type == 'Stamp' && args && args.into) {
+      args.into.Content = {
+        Type: 'Stamp',
+        id: (item.data.Stamp && item.data.Stamp.id),
+        bounds: {
+          x: item.bounds.topLeft.x,
+          y: item.bounds.topLeft.y,
+          width: item.bounds.width,
+          height: item.bounds.height
+        }
+      };
+      args.into.Definition = $.extend({}, item.data.Stamp);
+      let svgLoader = this.getStampSVG(item);
+      svgLoader.then((svgData) => {
+        args.into.Definition.symbol_def = svgData;
+      });
+      args.into.Loaders = [svgLoader];
     }
   }
 
@@ -286,6 +363,11 @@ export default class StampPalette extends Palette {
         this.importStamp(item, args);
       }, 'Stamp.Import');
     }
+    if (!this.eventHandlers.ContentExport) {
+      this.eventHandlers.ContentExport = this.SL.Paper.on('Content.Export', StampFilter, (args, item) => {
+        this.exportStamp(item, args);
+      }, 'Stamp.Export');
+    }
   }
   resetEventHandlers() {
     if (!this.initialized || !this.eventHandlers) {
@@ -310,6 +392,11 @@ export default class StampPalette extends Palette {
       this.SL.Paper.off('Content.Import', this.eventHandlers.ContentImport.id);
       delete this.eventHandlers.ContentImport;
       this.eventHandlers.ContentImport = undefined;
+    }
+    if (this.eventHandlers.ContentExport) {
+      this.SL.Paper.off('Content.Export', this.eventHandlers.ContentExport.id);
+      delete this.eventHandlers.ContentExport;
+      this.eventHandlers.ContentExport = undefined;
     }
   }
   registerSnappers() {
