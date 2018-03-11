d721c10426806d5ad79c9c80f4f0f29144636d46
Pat Long <plong00@gmail.com>
2018-03-11 03:06:39 -0600

    Adds dataURL option to SaveDrawing operation

--- src/operation/ops/save-drawing.js
+++ src/operation/ops/save-drawing.js
@@ -34,7 +34,9 @@ export class SaveDrawing extends Operation {
       let result = {
         id: this.getSaveID(args),
         blob: undefined,
-        type: undefined
+        type: undefined,
+        mimeType: undefined,
+        quality: undefined
       };
       let saving = false;
       try {
@@ -49,13 +51,40 @@ export class SaveDrawing extends Operation {
           }
           this.SL.Paper.emit('Content.Hide', eventArgs);
           saving = true;
+          let type = (args.type || 'png');
+          let mimeType = (args.mimeType || 'image/png');
+          let quality = args.quality;
           setTimeout(() => {
-            this.SL.canvas[0].toBlob((blob) => {
-              result.blob = blob;
-              result.type = 'png';
-              this.SL.Paper.emit('Content.Unhide', eventArgs);
-              resolve(result);
-            }, 'image/png');
+            let savers = [];
+            if (args.download) {
+              let download = new Promise((resolveDownload, rejectDownload) => {
+                this.SL.canvas[0].toBlob((blob) => {
+                  result.blob = blob;
+                  resolveDownload(result);
+                }, mimeType, quality);
+              });
+              savers.push(download);
+            }
+            if (args.dataURL) {
+              let getDataURL = new Promise((resolveDataURL, rejectDataURL) => {
+                result.dataURL = this.SL.canvas[0].toDataURL(mimeType, quality);
+                resolveDataURL(result);
+              });
+              savers.push(getDataURL);
+            }
+
+            Promise.all(savers)
+              .then(() => {
+                result.type = type;
+                result.mimeType = mimeType;
+                this.SL.Paper.emit('Content.Unhide', eventArgs);
+                resolve(result);
+              })
+              .catch(() => {
+                // same as .then() because Promise.finally not supported by all browsers
+                resolve(result);
+              });
+            
           }, 50);
         }
       }
@@ -81,9 +110,24 @@ export class SaveDrawing extends Operation {
       });
     });
   }
+  getDataURL(args) {
+    return new Promise((resolve, reject) => {
+      this.save(args).then((data) => {
+        if (data) {
+          resolve(data.dataURL);
+        }
+        else {
+          reject('No data was exported.');
+        }
+      });
+    });
+  }
   run(args) {
     if (args.download) {
       return this.saveDownload(args);
+    }
+    if (args.dataURL) {
+      return this.getDataURL(args);
     }
     return false;
   }
